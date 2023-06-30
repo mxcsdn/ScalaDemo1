92f94fc0e0e9ec926fffbc688584a7d7de0fdfd0
mxcsdn <1211823287@qq.com>
2023-06-30 11:18:43 +0800

    implicit转换

--- src/main/scala/com/atguigu/Hello.scala
+++ src/main/scala/com/atguigu/Hello.scala
@@ -1,16 +1,36 @@
 package com.atguigu
 
+import java.time.LocalDate
+
 object Hello {
-  def main(args: Array[String]): Unit = {
     var a: Int = 3
-    println("hello")
     println("测试12")
-    /*
+    def apply(age: Int,name: String,sex: String): Hello = new Hello(age)//暂时一般是固定写法
+}
+class Hello(age:Int){
 
-     */
-    /**
-     *
-     */
+}
+
+object Demo{
+  implicit def int2RichDate(day : Int) : RichDate= new RichDate(day : Int)
+  def main(args: Array[String]): Unit = {
+    val value = Hello(12, "mx", "man");//可以传多个参数
+    val ago : String = "ago"
+    val later : String = "later"
+    2 days ago// 将Int转换成RichDate类型。2实参，days方法，ago变量
+    val str: String = 2 days later //
+    println(2 days ago)
+    println(2 days later)
   }
+}
 
+class RichDate(val day : Int){
+  def days(when : String) :String ={
+      val nowDate = LocalDate.now()
+      if (when == "ago"){
+          nowDate.minusDays(day).toString
+      }else{
+          nowDate.plusDays(day).toString
+      }
+  }
 }
